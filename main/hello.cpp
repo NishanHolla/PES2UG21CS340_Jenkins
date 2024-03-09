e0df1b2354e86a91fb40180b29f9ae7b4fdfc744
Nishan Holla <hollanishan@gmail.com>
2024-03-09 11:15:37 +0530

    change dagain

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, this is Water\n";
+  cout << "Hello, this is Nishan\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
